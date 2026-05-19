61292617ffc23d25e9c013cb2e30d98d28a531ff
Rahul Jha <rjha5516@gmail.com>
2026-05-19 12:51:52 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        n = len(nums)
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+        for num in hashmap:
+            if hashmap[num] > n//2:
+                return num
