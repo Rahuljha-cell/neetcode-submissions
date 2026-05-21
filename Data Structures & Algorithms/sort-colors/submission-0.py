c240c6216743bfce882f5cafa4e98f2fec9d62c6
Rahul Jha <rjha5516@gmail.com>
2026-05-21 11:48:45 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def quicksort(left, right):
+            if left > right:
+                return 
+            pivot = nums[(left + right)//2]
+            i = left
+            j = right
+            while (i <= j):
+                while (nums[i] < pivot):
+                    i += 1
+                while (nums[j] > pivot):
+                    j -= 1
+                if i <= j:
+                    #swap
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                    j -= 1
+            quicksort(left, j)
+            quicksort(i, right)
+        quicksort(0, len(nums) - 1)
