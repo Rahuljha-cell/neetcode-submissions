399ca63d8a2c08c129c30e442feb151b3f5a443f
Rahul Jha <rjha5516@gmail.com>
2026-05-28 20:54:55 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        hashmap = defaultdict(int)
+        res = []
+        for n in nums:
+            hashmap[n] += 1
+        for key, val in hashmap.items():
+            if val > len(nums)//3:
+                res.append(key)
+        return res
